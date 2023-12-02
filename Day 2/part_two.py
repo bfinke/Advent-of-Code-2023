34c2100c7cae73ae5b633268da342b9b88e8a432
Benjamin Finke <143861239+bfinke@users.noreply.github.com>
2023-12-02 01:12:22 -0700

    simplified

--- Day 2/part_two.py
+++ Day 2/part_two.py
@@ -3,26 +3,16 @@ with open('puzzle.txt') as f:
     for line in f:
         game, items = line.split(':')
         items = items.split(';')
-        blue = 0
-        green = 0
-        red = 0
+        blue, green, red = [0, 0, 0]
         for item in items:
-            marbles = item.split(', ')
+            marbles = item.split(',')
             for i in marbles:
-                if 'blue' in i:
-                    i = i.strip()
-                    number, color = i.split(' ')
-                    if int(number) > blue:
-                        blue = int(number)
-                if 'green' in i:
-                    i = i.strip()
-                    number, color = i.split(' ')
-                    if int(number) > green:
-                        green = int(number)
-                if 'red' in i:
-                    i = i.strip()
-                    number, color = i.split(' ')
-                    if int(number) > red:
-                        red = int(number)
+                number, color = i.strip().split(' ')
+                if color == 'blue' and int(number) > blue:
+                    blue = int(number)
+                if color == 'green' and int(number) > green:
+                    green = int(number)
+                if color == 'red' and int(number) > red:
+                    red = int(number)
         totals.append(red * blue * green)
 print(sum(totals))
