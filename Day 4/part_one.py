8718dd452bea547442dea49cbe56fed312b3634a
Benjamin Finke <143861239+bfinke@users.noreply.github.com>
2023-12-04 00:55:39 -0700

    simplified

--- Day 4/part_one.py
+++ Day 4/part_one.py
@@ -6,10 +6,7 @@ with open('puzzle.txt') as f:
         win_nums, have_nums = win_nums.split(' '), have_nums.split(' ')
         winning = [i.strip() for i in win_nums if i != '']
         haves = [i.strip() for i in have_nums if i != '']
-        count = 0
-        for nums in haves:
-            if nums in winning:
-                count += 1
+        count = sum([1 for nums in haves if nums in winning])
         if count != 0:
             wins.append(2 ** (count - 1))
 print(sum(wins))
--- Day 4/part_two.py
+++ Day 4/part_two.py
@@ -2,9 +2,8 @@ def total_wins(init_card, card_wins, dict):
     wins = 1
     if card_wins == 0:
         return 1
-    card = init_card
     for i in range(1, card_wins + 1):
-        wins += total_wins(card + i, dict[card + i], dict)
+        wins += total_wins(init_card + i, dict[init_card + i], dict)
     return wins
 
 
@@ -19,12 +18,5 @@ with open('puzzle.txt') as f:
         win_nums, have_nums = win_nums.split(' '), have_nums.split(' ')
         winning = [i.strip() for i in win_nums if i != '']
         haves = [i.strip() for i in have_nums if i != '']
-        count = 0
-        for nums in haves:
-            if nums in winning:
-                count += 1
-        card_dict[int(card[-1])] = count
-totals = []
-for k, v in card_dict.items():
-    totals.append(total_wins(k, v, card_dict))
-print(sum(totals))
+        card_dict[int(card[-1])] = sum([1 for nums in haves if nums in winning])
+print(sum([total_wins(k, v, card_dict) for k, v in card_dict.items()]))
