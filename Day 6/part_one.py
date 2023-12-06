b5011b59fcd3c2f85038ca47fa49ce78479cecd5
Benjamin Finke <143861239+bfinke@users.noreply.github.com>
2023-12-06 15:52:24 -0700

    tabbed

--- Day 6/part_one.py
+++ Day 6/part_one.py
@@ -2,12 +2,12 @@ import math
 
 with open('puzzle.txt') as f:
     time, distance = [line.strip('\n').split(':').pop(1).strip().split() for line in f]
-races = [[int(time[i]), int(distance[i])] for i in range(len(time))]
-record = []
-for t, d in races:
-    count = 0
-    for m in range(t):
-        if (t - m) * m > d:
-            count += 1
-    record.append(count)
-print(math.prod(record))
+    races = [[int(time[i]), int(distance[i])] for i in range(len(time))]
+    record = []
+    for t, d in races:
+        count = 0
+        for m in range(t):
+            if (t - m) * m > d:
+                count += 1
+        record.append(count)
+    print(math.prod(record))
--- Day 6/part_two.py
+++ Day 6/part_two.py
@@ -1,8 +1,8 @@
 with open('puzzle.txt') as f:
     time, distance = [line.strip('\n').split(':').pop(1).strip().split() for line in f]
-time, distance = [int(''.join(time)), int(''.join(distance))]
-count = 0
-for m in range(time):
-    if (time - m) * m > distance:
-        count += 1
-print(count)
+    time, distance = [int(''.join(time)), int(''.join(distance))]
+    count = 0
+    for m in range(time):
+        if (time - m) * m > distance:
+            count += 1
+    print(count)
